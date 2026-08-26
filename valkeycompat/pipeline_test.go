5d1a952509860c4dd5fbbde0845dedde464cff2d
Vardhaman Viroje <vardhamanviroje@Vardhamans-Mac-mini.local>
2026-08-26 13:10:36 +0530

    base code added for SlowlogLen

--- valkeycompat/pipeline_test.go
+++ valkeycompat/pipeline_test.go
@@ -624,6 +624,7 @@ func TestPipeliner(t *testing.T) {
 		p.JSONType(ctx, "1", "1")
 		p.SlaveOf(ctx, "NO", "ONE")
 		p.SlowLogGet(ctx, 1)
+		p.SlowLogLen(ctx)
 		p.SlowLogReset(ctx)
 		p.ClusterMyShardID(ctx)
 		p.ModuleLoadex(ctx, &ModuleLoadexConfig{
@@ -632,7 +633,7 @@ func TestPipeliner(t *testing.T) {
 			Args: []any{"1", "2"},
 		})
 
-		if n := len(p.rets); n != 491 {
+		if n := len(p.rets); n != 492 {
 			t.Fatalf("unexpected pipeline calls: %v", n)
 		}
 		for i, cmd := range p.rets {
@@ -1160,6 +1161,7 @@ var golden = `[
     ["JSON.TYPE","1","1"],
     ["SLAVEOF","NO","ONE"],
     ["SLOWLOG","GET","1"],
+	["SLOWLOG","LEN"],
     ["SLOWLOG","RESET"],
     ["CLUSTER","MYSHARDID"],
     ["MODULE","LOADEX","/","CONFIG","k","v","ARGS","1","2"]
